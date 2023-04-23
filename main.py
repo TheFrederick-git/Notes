11a5bbe40495e68eb12768820b9e644f79f04700
TheFrederick <thefrederick.dev@gmail.com>
2023-04-23 23:08:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-"""Hlavní soubor aplikace"""
+"""
+Poznámkovač
+By TheFrederick#8776
+23.04.2023
+"""
 
-from flask import Flask, render_template, redirect, request, Response
-from control import get_notes, add_note, remove_note
+from flask import Flask, render_template, request
+from control import get_note, get_notes, add_note, remove_note, init_db
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 
@@ -18,30 +22,31 @@ def create_note() -> str:
     return render_template("new.html")
 
 @app.route("/show/<int:note_id>")
-def show_note(note_id: str) -> str:
+def show_note(note_id:str) -> str:
     """Otevře poznámku"""
 
-    return render_template("show.html", row=get_notes(True, note_id))
+    return render_template("show.html", row=get_note(note_id))
 
-@app.route("/add", methods = ['POST'])
-def db_add() -> Response:
+@app.route("/add", methods = ["POST"])
+def db_add() -> str:
     """Post metoda určena pro zápis do databáze"""
 
     add_note(request.form["name"], request.form["text"])
-    return redirect("/")
+    return render_template("index.html", rows=get_notes())
 
 @app.route("/remove/<int:note_id>")
-def db_remove(note_id: str) -> Response:
+def db_remove(note_id:str) -> str:
     """Post metoda určena pro odstranění dat z databáze"""
 
     remove_note(note_id)
-    return redirect("/")
+    return render_template("index.html", rows=get_notes())
 
 @app.errorhandler(Exception)
-def show_error(code: str) -> str:
+def show_error(code:str) -> str:
     """Vrátí stránku s kočičím obrázkem chyby"""
 
     return render_template("error.html", code=str(code)[:3])
 
 if __name__ == "__main__":
+    init_db()
     app.run()
